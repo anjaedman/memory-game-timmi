57ad2b0e844f85af198b5818fce76322e0adb428
anjaedman <113342171+anjaedman@users.noreply.github.com>
2023-01-25 15:44:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+console.log("Funkar!");
+
+
+
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let lockBoard = false; 
+let firstCard;
+let secondCard;
+
+
+
+function flipCard() {
+
+if (lockBoard) return; 
+if (this === firstCard) return; 
+this.classList.add("flip");
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+}
+secondCard = this;
+checkFormatch();
+}
+
+function checkFormatch() {
+
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    } 
+    unFlipCards();
+
+}
+
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    resetBoard(); 
+}
+
+function unFlipCards() {
+        lockBoard = true; 
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+
+      
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+
+  (function shuffle() {
+    cards.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPos;
+    });
+  })();
+ 
+
+cards.forEach(card => card.addEventListener("click", flipCard));
+
